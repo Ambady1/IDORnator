dd9bdb79bd06575b0b0e50f88ba4c65f7dc72813
Ambady <cssreedeep@gmail.com>
2024-12-10 11:06:17 +0530

    Gemini api-incomplete

--- payload.py
+++ payload.py
@@ -1,30 +1,25 @@
 from groq import Groq
 from process_Form import process_form
+from openai import OpenAI
+import google.generativeai as genai
 
-
-def generate_payloads(url,param_value):
+def generate_payloads(url, key_element):
     """
-    Uses Groq API to dynamically generate potential payloads.
+    Uses chatgpt API to dynamically generate potential payloads.
     """
-    client = Groq(api_key="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")
-    response = client.chat.completions.create(
-        model="llama-3.1-70b-versatile",
-        messages=[
-            {
-                "role": "user",
-                "content": (
-                    
-                    f"""
-In a legal pentesting scenario, for testing IDOR vulnerability, I have the following url : '{url}'.
-Here '{param_value}' being the key element suscepted for IDOR testing, what could be the possible payloads I can use? Give a list of 15 urls with the payload without any further explanations
-"""
+    genai.configure(api_key="AIzaSyDW8Kxv6cphr6GTWUFwPeJFSgro1iaC95M")
 
-                )
-            }
-        ]
-    )
+    query = f"In a legal pentesting scenario, for testing IDOR vulnerability, I have the following url : {url} " \
+        f"Here {key_element} being the key element suscepted for IDOR testing, what could be the possible payloads I can use? " \
+        "Give a list of 15 urls with the payload without any further explanations"
+    
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(query)
+    
     try:
-        payloads = response.choices[0].message.content.split('\n')  # Assume response contains newline-separated payloads
+        # Assume response contains newline-separated payloads
+        payloads = response.choices[0].message.content.split('\n')
+        print(payloads)
         return [p.strip() for p in payloads if p.strip()]
     except Exception as e:
         print(f"Error generating payloads: {e}")
